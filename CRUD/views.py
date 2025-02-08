7685f186cf9a760aaed8995e63a8518b34f32a2a
Marcuss202 <markussozols25@gmail.com>
2025-02-08 23:27:41 +0200

    full dynamic CRUD

--- CRUD/views.py
+++ CRUD/views.py
@@ -34,13 +34,54 @@ def getTasks(request, table_name):
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-# @api_view(['POST'])
-# def createTask(request):
-#     serializer = TaskSerializer(data=request.data)
-#     if serializer.is_valid():
-#         serializer.save()
-#     return Response(serializer.data)
+@api_view(['POST'])
+def createObject(request, table_name):
+    try:
+        model = apps.get_model('CRUD', table_name)
+        if not model:
+            return Response({"error": "Invalid table name"}, status=status.HTTP_400_BAD_REQUEST)
 
+        serializer_class = SERIALIZER_MAPPING.get(table_name)
+        if not serializer_class:
+            return Response({"error": "No serializer found for this model"}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def details(request, table_name, pk):
+    try:
+        model = apps.get_model('CRUD', table_name)
+        if not model:
+            return Response({"error": "Invalid table name"}, status=status.HTTP_400_BAD_REQUEST)
+
+        tasks = model.objects.get(pk=pk)
+        serializer_class = SERIALIZER_MAPPING.get(table_name)
+        if not serializer_class:
+            return Response({"error": "No serializer found for this model"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if request.method == 'GET':
+            serializer = serializer_class(tasks)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        elif request.method == 'PUT':
+            serializer = serializer_class(tasks, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        elif request.method == 'DELETE':
+            tasks.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 # @api_view(['GET', 'PUT', 'DELETE'])
 # def task_detail(request, pk):
