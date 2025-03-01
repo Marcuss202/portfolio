f9811d2b118402fadda5d12473f51e2f37516de3
Marcuss202 <markussozols25@gmail.com>
2025-03-02 01:22:51 +0200

    vite configs updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-  ]
-})
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//   ]
+// })
 
+
+export default defineConfig({
+  base: "./",
+  build: {
+    outDir: "dist",
+  },
+});
